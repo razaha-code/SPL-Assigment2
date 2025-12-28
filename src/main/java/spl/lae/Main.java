7ab49f49283009d654a08c98c9cf2ab07454ea54
Raz Aharon <razaha@post.bgu.ac.il>
2025-12-28 23:00:11 +0200

    pushpushpush

--- src/main/java/spl/lae/Main.java
+++ src/main/java/spl/lae/Main.java
@@ -1,10 +1,30 @@
 package spl.lae;
 import java.io.IOException;
 
+import parser.ComputationNode;
+import parser.OutputWriter;
+
 public class Main {
     public static void main(String[] args) throws IOException {
       // TODO: main
+      LinearAlgebraEngine engine = new LinearAlgebraEngine(4);
+      System.out.println(engine.getWorkerReport());
+      ComputationNode answerNode = null;
+      OutputWriter outputWriter = new OutputWriter();
+      parser.InputParser parserworker = new parser.InputParser();
+      try { 
+          parser.ComputationNode root = parserworker.parse("input.json");
+          answerNode = engine.run(root);
+          System.out.println(engine.getWorkerReport());
+      } catch (java.text.ParseException e) {
 
+          outputWriter.write("Error parsing input: " + e.getMessage(), "output.json");
+      }   
+
+      if (answerNode != null) {
+            double[][] answerMatrix = answerNode.getMatrix();
+            outputWriter.write(answerMatrix, "output.json");
+      }
   
     }
 }
